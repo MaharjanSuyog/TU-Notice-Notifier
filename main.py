74526129d069f98840f7748e2b0a3b0b2e3fd176
Galaxy-1110 <suyogmaharjan38@gmail.com>
2026-08-28 21:06:39 +0545

    Feat: Main lifespan for fastapi

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from database import check_redis_connection, engine
+from database import SessionLocal, check_redis_connection, engine, initDB, redis_client
+from scraper import run_scraper
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # init_db()
+    initDB()
     check_redis_connection()
+
+    db = SessionLocal()
+    try:
+        run_scraper(redis_client=redis_client, db=db)
+    finally:
+        db.close()
 
     yield
 
     engine.dispose()
+    print("Application shutting down")
 
 
 app = FastAPI(title="TU NOTICE TRACKER", lifespan=lifespan)
