dd8f04cdc64196f16d315d4ed7cdca68adb021d2
Galaxy-1110 <suyogmaharjan38@gmail.com>
2026-09-06 19:07:04 +0545

    feat: Authentication via GOOGLE frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,7 +6,7 @@ import {
 	Plus_Jakarta_Sans,
 } from "next/font/google";
 import "./globals.css";
-
+import { AuthProvider } from "@/utils/AuthContext";
 const manrope = Manrope({
 	subsets: ["latin"],
 	variable: "--font-manrope",
@@ -40,7 +40,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 		<html
 			lang="en"
 			className={`${manrope.variable} ${spaceMono.variable} ${caveat.variable} ${pjs.variable} h-full antialiased`}>
-			<body className="min-h-full flex flex-col">{children}</body>
+			<body className="min-h-full flex flex-col">
+				<AuthProvider>{children}</AuthProvider>
+			</body>
 		</html>
 	);
 }
