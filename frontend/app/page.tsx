44184b0175c46ba802a688014cb63ca653313234
Galaxy-1110 <suyogmaharjan38@gmail.com>
2026-09-07 22:20:46 +0545

    fix: build issue bcz of lack of Suspense element

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import Card from "@/components/Card";
 import Pagination from "@/components/Pagination";
 import TagFilter from "@/components/TagFilter";
@@ -52,55 +52,64 @@ export default function Home() {
 		window.scrollTo({ top: 0, behavior: "smooth" });
 	};
 	return (
-		<div className="flex flex-col flex-1 items-center mt-14 font-sans dark:bg-slate min-h-screen">
-			<header className="flex items-end justify-end w-full mr-10 ">
-				<LoginButton />
-			</header>
-			<main className="w-3/4 flex flex-col gap-10">
-				<span>
-					<h1 className="font-handwritten font-extrabold text-8xl ">
-						RECENT NOTICES
-					</h1>
-					<span className="font-mono text-muted">watching: iost.tu.edu.np</span>
-				</span>
-
-				<TagFilter
-					activeTags={activeTags}
-					onClear={clearTags}
-					onToggle={toggleTag}
-				/>
-				<div className="mb-10 ">
-					{loading || noticeData === null ? (
-						<p className="font-mono text-muted">Loading.... </p>
-					) : error ? (
-						<p className="font-mono text-error">{error}</p>
-					) : noticeData.notices.length === 0 ? (
-						<p className="font-mono text-muted">No notices found.</p>
-					) : (
-						<>
-							{noticeData?.total_pages > 1 && (
-								<Pagination
-									page={page}
-									totalPages={noticeData.total_pages}
-									onPageChange={handlePageChange}
-								/>
-							)}
-							<div className="flex flex-col divide-y divide-muted/30 gap-2 mt-10">
-								{noticeData.notices.map((notice) => (
-									<Card key={notice.notice_id} notice={notice} />
-								))}
-							</div>
-							{noticeData.total_pages > 1 && (
-								<Pagination
-									page={page}
-									totalPages={noticeData.total_pages}
-									onPageChange={handlePageChange}
-								/>
-							)}
-						</>
-					)}
+		<Suspense
+			fallback={
+				<div className="flex justify-center mt-20 font-mono">
+					Loading notices...
 				</div>
-			</main>
-		</div>
+			}>
+			<div className="flex flex-col flex-1 items-center mt-14 font-sans dark:bg-slate min-h-screen">
+				<header className="flex items-end justify-end w-full mr-10 ">
+					<LoginButton />
+				</header>
+				<main className="w-3/4 flex flex-col gap-10">
+					<span>
+						<h1 className="font-handwritten font-extrabold text-8xl ">
+							RECENT NOTICES
+						</h1>
+						<span className="font-mono text-muted">
+							watching: iost.tu.edu.np
+						</span>
+					</span>
+
+					<TagFilter
+						activeTags={activeTags}
+						onClear={clearTags}
+						onToggle={toggleTag}
+					/>
+					<div className="mb-10 ">
+						{loading || noticeData === null ? (
+							<p className="font-mono text-muted">Loading.... </p>
+						) : error ? (
+							<p className="font-mono text-error">{error}</p>
+						) : noticeData.notices.length === 0 ? (
+							<p className="font-mono text-muted">No notices found.</p>
+						) : (
+							<>
+								{noticeData?.total_pages > 1 && (
+									<Pagination
+										page={page}
+										totalPages={noticeData.total_pages}
+										onPageChange={handlePageChange}
+									/>
+								)}
+								<div className="flex flex-col divide-y divide-muted/30 gap-2 mt-10">
+									{noticeData.notices.map((notice) => (
+										<Card key={notice.notice_id} notice={notice} />
+									))}
+								</div>
+								{noticeData.total_pages > 1 && (
+									<Pagination
+										page={page}
+										totalPages={noticeData.total_pages}
+										onPageChange={handlePageChange}
+									/>
+								)}
+							</>
+						)}
+					</div>
+				</main>
+			</div>{" "}
+		</Suspense>
 	);
 }
